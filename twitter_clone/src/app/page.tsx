bb5ce72b87164dd3b6a292fe945b7cb095bb466c
Nicolas Suarez <brayansuarezmongui@gmail.com>
2023-08-27 14:38:13 -0500

    Create users, security routes and GUI with NextUI

--- twitter_clone/src/app/page.tsx
+++ twitter_clone/src/app/page.tsx
@@ -1,18 +1,48 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
-import { AuthButton } from './components/auth-button'
+import { AuthButtonServer } from '@/app/components/auth-button-server'
+import { redirect } from 'next/navigation'
+import PostCard from './components/post-cards'
 
 export default async function Home () {
   const supabase = createServerComponentClient({ cookies })
-  const { data: posts } = await supabase.from('posts').select('*')
+  const { data: { session } } = await supabase.auth.getSession()
+
+  if (session === null) {
+    redirect('/login')
+  }
+
+  const { data: posts } = await supabase
+    .from('posts')
+    .select('*, user: users(name, avatar_url, user_name)')
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <AuthButton/>
-      <pre>
-        {
-          JSON.stringify(posts, null, 2)
-        }
-      </pre>
+      <AuthButtonServer/>
+
+      {
+        posts?.map(post => {
+          const {
+            id,
+            user,
+            content
+          } = post
+
+          const {
+            user_name: userName,
+            name: userFullName,
+            avatar_url: avatarUrl
+          } = user
+          return (
+            <PostCard
+              avatarUrl={avatarUrl}
+              content={content}
+              key={id}
+              userFullName={userFullName}
+              userName={userName}
+            />
+          )
+        })
+      }
     </main>
   )
 }
