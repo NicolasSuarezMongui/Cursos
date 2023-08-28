b29d5417ed9b215f8c2e8ba6b4242a3c6d2d24bb
Nicolas Suarez <brayansuarezmongui@gmail.com>
2023-08-28 01:53:01 -0500

    GUI improvements, functions and trigger with supabase, Send and save posts

--- twitter_clone/src/app/components/post-cards.tsx
+++ twitter_clone/src/app/components/post-cards.tsx
@@ -1,6 +1,7 @@
 'use client'
-import { Card, CardHeader, CardBody, CardFooter, Avatar, Button } from '@nextui-org/react'
+import { Card, CardHeader, CardBody, CardFooter, Avatar } from '@nextui-org/react'
 import Link from 'next/link'
+import { IconMessageCircle, IconHeart, IconRepeat } from '@tabler/icons-react'
 
 export default function PostCard ({
   userFullName,
@@ -14,7 +15,7 @@ export default function PostCard ({
   content: string
 }) {
   return (
-    <Card className="max-w-[340px]">
+    <Card className="shadow-none bg-transparent hover:bg-neutral-800 rounded-none cursor-pointer transition border-b border-white/20">
       <CardHeader className="justify-between">
         <div className="flex gap-x-2">
           <Link href={`/${userName}`}>
@@ -25,21 +26,22 @@ export default function PostCard ({
               <h5 className="text-small tracking-tight text-default-400">@{userName}</h5>
           </div>
         </div>
-    </CardHeader>
-    <CardBody className="px-3 py-0 text-small text-white">
-      <p>
-        {content}
-      </p>
-    </CardBody>
-    <CardFooter className="gap-3">
-      <div className="flex gap-1">
-        <p className="font-semibold text-default-400 text-small">4</p>
-        <p className=" text-default-400 text-small">Following</p>
-      </div>
-      <div className="flex gap-1">
-        <p className="font-semibold text-default-400 text-small">97.1K</p>
-        <p className="text-default-400 text-small">Followers</p>
-      </div>
+      </CardHeader>
+      <CardBody className="px-3 py-0 text-xs text-white">
+        <p>
+          {content}
+        </p>
+      </CardBody>
+      <CardFooter className="gap-3">
+        <button>
+          <IconMessageCircle className="w-4 h-4" />
+        </button>
+        <button>
+          <IconHeart className="w-4 h-4" />
+        </button>
+        <button>
+          <IconRepeat className="w-4 h-4" />
+        </button>
       </CardFooter>
     </Card>
   )
--- twitter_clone/src/app/page.tsx
+++ twitter_clone/src/app/page.tsx
@@ -2,10 +2,12 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { AuthButtonServer } from '@/app/components/auth-button-server'
 import { redirect } from 'next/navigation'
-import PostCard from './components/post-cards'
+import { PostLists } from './components/posts-list'
+import { type Database } from './types/database'
+import { ComposePost } from './components/compose-post'
 
 export default async function Home () {
-  const supabase = createServerComponentClient({ cookies })
+  const supabase = createServerComponentClient<Database>({ cookies })
   const { data: { session } } = await supabase.auth.getSession()
 
   if (session === null) {
@@ -15,34 +17,15 @@ export default async function Home () {
   const { data: posts } = await supabase
     .from('posts')
     .select('*, user: users(name, avatar_url, user_name)')
+    .order('created_at', { ascending: false })
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <AuthButtonServer/>
+    <main className="flex min-h-screen flex-col items-center justify-between">
 
-      {
-        posts?.map(post => {
-          const {
-            id,
-            user,
-            content
-          } = post
-
-          const {
-            user_name: userName,
-            name: userFullName,
-            avatar_url: avatarUrl
-          } = user
-          return (
-            <PostCard
-              avatarUrl={avatarUrl}
-              content={content}
-              key={id}
-              userFullName={userFullName}
-              userName={userName}
-            />
-          )
-        })
-      }
+      <section className="max-w-[600px] w-full mx-auto border-l border-r border-white/20 min-h-screen">
+        <ComposePost userAvatarUrl={session?.user?.user_metadata?.avatar_url}/>
+        <PostLists posts={posts}/>
+      </section>
+      <AuthButtonServer />
     </main>
   )
 }
